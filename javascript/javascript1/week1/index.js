5e799330752aea437861fa7dea972435e4308a39
faheem102 <faheemhassan1985@gmail.com>
2021-10-27 01:03:05 +0200

    completed dog age calculator

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -8,4 +8,11 @@ const dogYearOfBirth = 2020;
 const dogYearFuture = 2045;
 const humanYear = dogYearFuture - dogYearOfBirth;
 const dogYear = (dogYearFuture - dogYearOfBirth) * 7;
-let shouldShowResultInDogYears = true;
+let shouldShowResultInDogYears = false;
+if (shouldShowResultInDogYears === true) {
+    console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
+
+}
+else {
+    console.log(`Your dog will be ${humanYear} human years old in ${dogYearFuture}`);
+}
