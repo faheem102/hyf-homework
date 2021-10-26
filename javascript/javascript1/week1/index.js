bd527b0645f8c347360633bda66979aa3ee2e4eb
faheem102 <faheemhassan1985@gmail.com>
2021-10-27 01:24:07 +0200

    changed it to string concatenation

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -2,7 +2,7 @@ console.log("Age-ify" + "A future age calculater.");
 const yearOfBirth = 1985;
 const yearFuture = 2045;
 const age = yearFuture - yearOfBirth;
-console.log(`You will be ${age} in ${yearFuture}.`);
+console.log("You will be " + age  + " years in " + yearFuture + ".");
 console.log("Goodboy-Oldboy" + " (A dog age Calculator)");
 const dogYearOfBirth = 2020;
 const dogYearFuture = 2045;
@@ -16,3 +16,4 @@ if (shouldShowResultInDogYears === true) {
 else {
     console.log(`Your dog will be ${humanYear} human years old in ${dogYearFuture}`);
 }
+console.log("Housey Pricey" + " (A house price estimator)");
