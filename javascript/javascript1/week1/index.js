93d8ecf5d5cff48e3bc4a15b6f830d327d9c7da2
faheem102 <faheemhassan1985@gmail.com>
2021-10-27 00:39:20 +0200

    Age-ify calucator completed

---
+++ javascript/javascript1/week1/index.js
@@ -0,0 +1,5 @@
+console.log("Age-ify" + "A future age calculater.");
+const yearOfBirth = 1985;
+const yearFuture = 2045;
+const age = yearFuture - yearOfBirth;
+console.log(`You will be ${age} in ${yearFuture}.`)
