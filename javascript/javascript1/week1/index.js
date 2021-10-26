680d2db3d901c881eaee7d195a5a8a1a451c4197
faheem102 <faheemhassan1985@gmail.com>
2021-10-27 00:41:40 +0200

    semicolon was missing

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -2,4 +2,4 @@ console.log("Age-ify" + "A future age calculater.");
 const yearOfBirth = 1985;
 const yearFuture = 2045;
 const age = yearFuture - yearOfBirth;
-console.log(`You will be ${age} in ${yearFuture}.`)
+console.log(`You will be ${age} in ${yearFuture}.`);
