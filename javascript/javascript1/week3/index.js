a62162a128d0306b75ea432a59b202204f8bc45e
faheem102 <faheemhassan1985@gmail.com>
2021-11-13 22:18:32 +0100

    item array removal completed

---
+++ javascript/javascript1/week3/index.js
@@ -0,0 +1,16 @@
+//Step- 1: Item Array Removal
+
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  
+  // Write some code here
+  const nameToRemove = names.splice(1,1);
+  // Code done
