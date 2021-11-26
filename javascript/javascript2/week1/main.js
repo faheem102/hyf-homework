8744ee174186e059ece6030f82bc0ecf1cd05c09
faheem102 <faheemhassan1985@gmail.com>
2021-11-26 19:56:24 +0100

    shortest word finder

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -22,3 +22,75 @@ function renderProducts(product) {
     }
   }
   renderProducts();
+
+  //Spirited Animal Task
+
+  const spiritAnimals = ["The crying Butterfly", "The fullmoon Wolf", "The cunning Fox", "The roaring Lion", "The flying Hawk", "The aggresive Bear", "The elegant Swan", "The durable Camel", "The peacful Dove", "The lively Dolphin"]
+
+
+const bodyOfPage = document.querySelector("body");
+const nameInput = document.createElement("input");
+nameInput.setAttribute("type", "text");
+nameInput.setAttribute("placeholder", "Write your name here");
+nameInput.setAttribute("value", "");
+nameInput.setAttribute("id", "personName")
+const myButton = document.createElement("button");
+myButton.innerHTML = "Click Me!"
+const resultText = document.createElement("p");
+
+bodyOfPage.appendChild(nameInput);
+bodyOfPage.appendChild(myButton);
+bodyOfPage.appendChild(resultText);
+
+function toggleAnimalName(){
+    const getName = document.getElementById("personName").value;
+    console.log(getName);
+    if(getName.length<=0){
+        resultText.innerHTML = "Please provide your name to see the result";
+        
+        
+    }
+    else{
+        
+        const randomAnimals = spiritAnimals[Math.floor(Math.random()*spiritAnimals.length)];
+        resultText.innerHTML = `${getName}- ${randomAnimals}`
+        
+
+
+    }
+
+}
+
+function buttonClick(){
+    myButton.addEventListener("click", function(){
+        if(document.querySelector("#choice-button").checked){
+            toggleAnimalName();
+        }
+    });
+}
+function hoverEffect(){
+    nameInput.addEventListener("mouseover", function(){
+        if(document.querySelector("#choice-hover").checked){
+            toggleAnimalName();
+        }
+    });
+}
+buttonClick();
+hoverEffect();
+
+//Shortest word finder
+
+const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
+function shortestWord(a,b){
+    if(a.length<= b.length){
+        return a;
+
+    }
+    else{
+        return b;
+    }
+
+    
+    
+    }
+console.log (danishWords.reduce(shortestWord));
