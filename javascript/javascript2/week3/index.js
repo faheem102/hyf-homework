387eb63a8d495436121c7eef7f16eef61f386d66
faheem102 <faheemhassan1985@gmail.com>
2021-12-11 17:08:25 +0100

    fastest presser game completed

--- javascript/javascript2/week3/index.js
+++ javascript/javascript2/week3/index.js
@@ -135,3 +135,89 @@ const myData = {
 
 }
 myData.age(1985)
+
+//!--Fastest Presser Game--!
+
+
+let sCount = 0;
+let lCount = 0;
+var refreshIntervalId;
+var gameStarted = false;
+var confetti;
+function noOfGames() {
+
+  if (parseInt(document.getElementById("numberOfClicks").value) > 0)
+    StartGame();
+  else {
+    document.getElementById("error").innerHTML = "Set time (greater than or equal to 1 minute) and start game, before clicking!!";
+    return;
+  }
+}
+function eventListener(e) {
+    e = e || window.event;
+    console.log(e)
+  
+    if (!gameStarted)
+      return;
+  
+    if (e.keyCode == 115) {
+      sCount++
+      document.getElementById("pressS").innerHTML = sCount;
+    }
+    else if (e.keyCode == 108) {
+      lCount++
+      document.getElementById("pressL").innerHTML = lCount;
+    }
+  }
+  function StartGame() {
+    console.log("Called start")
+    sCount = 0;
+    lCount = 0;
+    gameStarted = true;
+    if (confetti)
+      confetti.clear();
+    const timeToGame = (parseInt(document.getElementById("numberOfClicks").value) * 1000);
+    document.getElementById("pressL").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("pressS").innerHTML = "";
+    document.getElementById("pressL").innerHTML = "";
+    document.getElementById("error").innerHTML = "";
+    document.getElementById("counter").innerHTML = "";
+  
+    document.onkeypress = eventListener;
+  
+    updateTimer(timeToGame);
+    setTimeout(() => {
+      finishGame();
+    }, timeToGame);
+  }
+  function updateTimer(timeToGame) {
+    timeToGame = timeToGame / 1000;
+    document.getElementById("counter").innerHTML = timeToGame-- + " seconds";
+    refreshIntervalId = setInterval(() => {
+      document.getElementById("counter").innerHTML = timeToGame-- + " seconds";
+    }, 1000);
+  }
+  function finishGame() {
+    gameStarted = false;
+    console.log("Finished")
+    document.removeEventListener('onkeypress', eventListener);
+    clearInterval(refreshIntervalId);
+  
+    if (sCount > lCount) {
+      document.getElementById("result").innerHTML = "Game over, player with key S won with " + (sCount) + " hits";
+      renderConfetti('my-canvasS');
+    } else if (lCount > sCount) {
+      document.getElementById("result").innerHTML = "Game over, player with key L won with " + (lCount) + " hits";
+      renderConfetti('my-canvasL');
+    } else if (sCount == lCount) {
+      document.getElementById("result").innerHTML = "Game over, Match drawn with equal hits";
+    }
+  
+  }
+  
+  function renderConfetti(canvas) {
+    var confettiSettings = { target: canvas, width: 200, height: 200 };
+    confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+  }
