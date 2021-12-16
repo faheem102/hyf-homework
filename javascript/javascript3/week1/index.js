9c47a01712bc490cdb7d21bd2d1ae1340548f881
faheem102 <faheemhassan1985@gmail.com>
2021-12-16 23:18:04 +0100

    weather api and its use

---
+++ javascript/javascript3/week1/index.js
@@ -0,0 +1,28 @@
+//!--Music JSON file--!
+    
+[
+    {
+      "Name": "Enrique Inglesias",
+      "image":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Robin_Wong_Enrique_013.JPG/220px-Robin_Wong_Enrique_013.JPG",
+      "Age":"46",
+      "Nationality": "Spain",
+      "favorite song": "Bailamos"
+
+
+    },
+    {
+      "Name": "Ricky Martin",
+      "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Ricky_Martin_in_store_appearance%2C_Sydney_Australia_%281%29.jpg/220px-Ricky_Martin_in_store_appearance%2C_Sydney_Australia_%281%29.jpg",
+      "Age": "49",
+      "Nationality": "Spain",
+      "favorite song": "Livin' la Vida Loca"
+
+    }
+  ]
+  // I have used openweather api and used it. it is a very cool api to fetch data about weather to use in own apps.
+  // This api is has different endpoints. If someone is looking for current weather data then the following api endpoint 
+  //should be used api.openweathermap.org/data/2.5/weather
+  // Along with above URL one need to use parameters like cityname, zip code, geolocation and api key as well
+  // one example of using this api with different parameters is following 
+  //'https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=2e67673623b242fce63358a81c88036e'
+  
