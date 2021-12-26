02788d707da0b36b00320403bad20a4f803d4f7f
faheem102 <faheemhassan1985@gmail.com>
2021-12-26 16:24:00 +0100

    seperate files for both apps

--- javascript/javascript3/week1/index.js
+++ javascript/javascript3/week1/index.js
@@ -92,34 +92,3 @@ myButton.addEventListener("click", () =>{
   } 
 })
 
-//!--Giphy App--!
-
-const gifKeyword = document.getElementById("giphy-keyword")
-const gifNumber = document.getElementById("giphy-number")
-const gifButton = document.getElementById("giphy-button")
-const gifDisplay = document.getElementById("giphy-display")
-
-
-
-gifButton.addEventListener("click", () =>{
-  const getWord = gifKeyword.value
-  const getNumber = gifNumber.value
-
-  const api = `https://api.giphy.com/v1/gifs/search?q=${getWord}&limit=${getNumber}&api_key=OVMdw9YMkayCQT986VoJOzNFS0SJd2M5`
-  fetch(api)
-  .then((response)=>{
-    return response.json()
-})
-  .then(obj =>{
-    console.log(obj);
-    const result = obj.data
-    result.forEach(element => {
-      let gifImages = document.createElement("img")
-      gifImages.src = element.images.fixed_height.webp
-      gifDisplay.appendChild(gifImages)
-    });
-  
-})
-
- })
-  
