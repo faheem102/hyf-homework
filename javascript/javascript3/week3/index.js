42f05a5fb8227d0c7c5755b77544de21708622d2
faheem102 <faheemhassan1985@gmail.com>
2022-01-29 10:27:37 +0100

    step-3 make some art

--- javascript/javascript3/week3/index.js
+++ javascript/javascript3/week3/index.js
@@ -23,7 +23,7 @@ const getMyCanvas = document.getElementById("mycanvas").getContext("2d");
     getMyCanvas.canvas.height = innerHeight;
 
 
-class Circle{
+class myCircle{
     constructor(x, y, r, startAngle, endAngle, fillColor){
         this.x = x
         this.y = y
@@ -32,7 +32,7 @@ class Circle{
         this.endAngle = endAngle
         this.fillColor = fillColor
     }
-    draw(){
+    drawCircles(){
         
         getMyCanvas.fillStyle = this.fillColor;
         getMyCanvas.lineWidth = 7;
@@ -46,15 +46,73 @@ class Circle{
     }
 }
 
-const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, "blue");
-c1.draw();
-const c2 = new Circle(100, 75, 20, 0, 2 * Math.PI, "red");
-c2.draw();
-const c3 = new Circle(225, 95, 20, 0, 2 * Math.PI, "orange");
-c3.draw();
-const c4 = new Circle(355, 115, 25, 0, 2 * Math.PI, "pink");
-c4.draw();
-const c5 = new Circle(465, 120, 30, 0, 2 * Math.PI, "black");
-c5.draw();
-const c6 = new Circle(595, 150, 40, 0, 2 * Math.PI, "yellow");
-c6.draw();
+const c1 = new myCircle(50, 50, 20, 0, 2 * Math.PI, "blue");
+c1.drawCircles();
+const c2 = new myCircle(100, 75, 20, 0, 2 * Math.PI, "red");
+c2.drawCircles();
+const c3 = new myCircle(225, 95, 20, 0, 2 * Math.PI, "orange");
+c3.drawCircles();
+const c4 = new myCircle(355, 115, 25, 0, 2 * Math.PI, "pink");
+c4.drawCircles();
+const c5 = new myCircle(465, 120, 30, 0, 2 * Math.PI, "black");
+c5.drawCircles();
+const c6 = new myCircle(595, 150, 40, 0, 2 * Math.PI, "yellow");
+c6.drawCircles();
+
+//Step--3 Make some art with circles
+
+
+// make a function to generate random colors for each circle 
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;;
+
+  }
+
+  // create a class and inside that class create a draw function in order to draw circles
+  
+class Circle{
+    constructor(x, y, r, startAngle, endAngle, fillColor){
+        this.x = x
+        this.y = y
+        this.r = r
+        this.startAngle = startAngle
+        this.endAngle = endAngle
+        this.fillColor = fillColor
+    
+        
+    }
+    
+    draw(){
+        
+        
+        getMyCanvas.fillStyle = this.fillColor;
+        getMyCanvas.lineWidth = 7;
+        getMyCanvas.strokeStyle = "violet"
+        getMyCanvas.beginPath()
+        getMyCanvas.arc(this.x, this.y, this.r, this.startAngle, this.endAngle);
+        getMyCanvas.stroke();
+        getMyCanvas.fill();
+        getMyCanvas.closePath()
+        
+    
+    }
+       
+}
+
+// draw several circles with random positions and radius and colors after 2 seconds
+        setInterval(() => {
+        getRandomColor();
+        const randomX= Math.floor(Math.random() * window.innerWidth);
+        const randomY= Math.floor(Math.random() * window.innerHeight);
+        const randomRadius= Math.floor(Math.random() * 50);
+        const circle= new Circle(randomX, randomY, randomRadius, 0, 2 * Math.PI, getRandomColor());
+        
+        circle.draw()
+        
+    }, 2000);
