826e78be9535493ed286c299081fd5f59c841033
faheem102 <faheemhassan1985@gmail.com>
2022-01-29 10:54:21 +0100

    products into shopping cart

--- javascript/javascript3/week3/index2.js
+++ javascript/javascript3/week3/index2.js
@@ -58,3 +58,99 @@ Promise.all([promise1, promise2, promise3]).then((data) => {
    returnList(data);
 
 })
+
+//Step--5 Products into shopping cart
+
+// create two classes one for product and other for shoppingcart
+
+class Product {
+    constructor(name, price) {
+      this.name = name;
+      this.price = price;
+    }
+  }
+  
+  class ShoppingCart {
+    constructor() {
+      this.products = [];
+    }
+
+    // inside the class create different functionality 
+  
+    addProduct(product) {
+      // Implement functionality here
+      this.products.push(product);
+    }
+  
+    removeProduct(product) {
+      // Implement functionality here
+      var indexOfProduct = this.products.indexOf(product);
+      this.products.splice(indexOfProduct, 1);
+      //delete this.products[product];
+    }
+  
+    searchProduct(productName) {
+      // Implement functionality here
+      var item = this.products.find((item) => {
+        return item.name === productName;
+      });
+      return item;
+    }
+  
+    getTotal() {
+      // Implement functionality here
+      return this.products.reduce((partSum, a) => partSum + a.price, 0);
+    }
+  
+    renderProducts() {
+      // Implement functionality here
+      var productsLs = "";
+      var productsDiv = document.querySelector("#products");
+      this.products.forEach((element) => {
+        productsLs += element.name;
+      });
+      productsDiv.innerHTML = productsLs;
+    }
+  
+    getUser(user) {
+      // Implement functionality here
+  
+      return fetch(" https://jsonplaceholder.typicode.com/users/" + 1).then(
+        (response2) => response2.json()
+      );
+    }
+  // using switch statements to turn DKK into other currency
+     convertCurrency(currency){
+  
+      switch (currency) {
+          case "DOllar":
+              return this.getTotal() * 6.5;
+              break;
+          case "PKR":
+              return this.getTotal() * 26.5;
+                  break;
+          default:
+              break;
+      }
+     }
+  }
+  
+  const shoppingCart = new ShoppingCart();
+  const flatscreen = new Product("flat-screen", 5000);
+  const laptop = new Product("Apple macAir", 10000);
+  shoppingCart.addProduct(flatscreen);
+  shoppingCart.addProduct(laptop);
+  shoppingCart.renderProducts();
+     var productReturned = shoppingCart.searchProduct("flat-screen");
+     console.log(productReturned)
+     //shoppingCart.removeProduct(productReturned);
+  console.log(shoppingCart.convertCurrency("PKR"))
+  
+  shoppingCart.getUser().then((user) => {
+    console.log(user);
+    document.querySelector("#user").innerHTML = user.name;
+    shoppingCart.renderProducts();
+    document.querySelector("#total").innerHTML =
+      "Total:" + shoppingCart.getTotal();
+  });
+  
