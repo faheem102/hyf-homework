94614d53b3cb9bef657c39dae51ea34feeea4da1
faheem102 <faheemhassan1985@gmail.com>
2022-02-11 10:59:41 +0100

    working with tasks queries

---
+++ databases/week2/hyf_lesson02queries.sql
@@ -0,0 +1,41 @@
+USE hyf_lesson02;
+
+SELECT *
+FROM task;
+
+insert into task (id, title, description, created, updated, due_date, status_id) values (36, 'Complete the Assignment', 'work on your recent hyf assignment', '2022-02-06 06:54:16', '2022-02-07 13:05:09', '2022-02-11 13:05:09' , 2);
+
+UPDATE task
+SET title = 'Eat your Dinner'
+WHERE id = 36;
+
+UPDATE task
+SET status_id = 3
+WHERE id = 36;
+
+
+DELETE FROM task
+WHERE id =36;
+
+SELECT *
+FROM user_task
+JOIN user ON user.id = user_task.user_id 
+WHERE user.email LIKE '%@spotify.com';
+
+SELECT *
+FROM user_task
+JOIN user ON user.id = user_task.user_id
+JOIN task ON task.id = user_task.task_id
+WHERE user.name = 'Donald Duck'
+AND task.status_id = 1;
+
+SELECT *
+FROM user_task
+JOIN user ON user.id = user_task.user_id
+JOIN task ON task.id = user_task.task_id
+WHERE user.name = 'Maryrose Meadows'
+AND month(task.created) = 9;
+
+SELECT month(created), COUNT(title)
+FROM task
+GROUP BY month(task.created);
